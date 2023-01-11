77e1ea2fc17d6fe99dd0a14b9059d63714d25cce
hi.youthinker <hi.youthinker@gmail.com>
2023-01-11 15:12:10 +0000

    get ixgbe/i40e private data

--- kernel/get_infos.c
+++ kernel/get_infos.c
@@ -8,6 +8,16 @@
 #include <linux/delay.h>
 #include <linux/netdevice.h>
 
+#define DRV_IXGBE
+
+/* for linux-4.15 */
+#ifdef DRV_IXGBE
+#include "include/ixgbe/ixgbe.h"
+#else
+#include "include/i40e/i40e.h"
+#include "include/i40e/i40e_diag.h"
+#endif
+
 static char *name;
 module_param(name, charp, 0);
 MODULE_PARM_DESC(name, "interface name");
@@ -31,14 +41,47 @@ static int __init my_debug_init(void)
 		printk("Can't find the interface %s\n", name);
 		return 0;
  	}
-	printk("dev %s: type: %u", dev->name, dev->type);
+
+	printk("dev %s: type: %u, features: %llx\n", dev->name, dev->type, dev->features);
+
+	if (dev->features & NETIF_F_SG) {
+		printk("NETIF_F_SG is on\n");
+	} else {
+		printk("NETIF_F_SG is off\n");
+	}
+
+	if (dev->features & NETIF_F_FRAGLIST) {
+		printk("NETIF_F_FRAGLIST is on\n");
+	} else {
+		printk("NETIF_F_FRAGLIST is off\n");
+	}
+
 	if (dev->netdev_ops) {
 		const struct net_device_ops *ops = dev->netdev_ops;
 
-		printk("xmit: %pf, ioctl: %p"
+		printk("xmit: %pf, ioctl: %p\n"
 			, ops->ndo_start_xmit, ops->ndo_do_ioctl);
 	}
-	printk("\n");
+
+#ifdef DRV_IXGBE
+	{
+		struct ixgbe_adapter *adapter;
+
+		adapter = netdev_priv(dev);
+		printk("atr_sample_rate: %u\n", adapter->atr_sample_rate);
+	}
+#else
+	{
+		struct i40e_netdev_priv *np;
+		struct i40e_vsi *vsi;
+
+		np = netdev_priv(dev);
+		vsi = np->vsi;
+
+		printk("vsi->rss_size: %u\n", vsi->rss_size);
+	}
+#endif
+
 	dev_put(dev);
  	return 0;
 }
